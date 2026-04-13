6b829e4377c5ab45935805920752afdd615e32d4
Srithan Yadav <arlasrithan@gmail.com>
2026-04-13 22:13:54 +0530

    Added localStorage feature

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+// Load saved skills when page opens
+window.onload = function() {
+    let skills = JSON.parse(localStorage.getItem("skills")) || [];
+
+    skills.forEach(skill => {
+        addSkillToList(skill);
+    });
+};
+
 function addSkill() {
     let input = document.getElementById("skillInput");
     let skill = input.value;
 
     if (skill === "") return;
 
-    let li = document.createElement("li");
-    li.textContent = skill;
+    addSkillToList(skill);
 
-    document.getElementById("skillList").appendChild(li);
+    let skills = JSON.parse(localStorage.getItem("skills")) || [];
+    skills.push(skill);
+    localStorage.setItem("skills", JSON.stringify(skills));
 
     input.value = "";
+}
+
+function addSkillToList(skill) {
+    let li = document.createElement("li");
+    li.textContent = skill;
+    document.getElementById("skillList").appendChild(li);
 }
